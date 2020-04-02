de7118bc4846b1a63b1183bc4ced67078431a250
Monar <pymonar@foxmail.com>
2020-04-02 21:09:09 +0800

    feat: 增加 element

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="/">主页</el-menu-item>
+      <el-menu-item index="/about">关于</el-menu-item>
+    </el-menu>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      activeIndex: '/'
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      this.$router.push(key)
+    }
+  }
+}
+</script>
 
