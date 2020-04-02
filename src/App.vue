f95b86fdddbb9910fb5ac292fdb901bb0c3561a8
Monar <pymonar@foxmail.com>
2020-04-02 21:14:59 +0800

    feat: 开始！

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="app">
+    <!-- header -->
+    <div id="header">
+      标题
+    </div>
+    <!-- 主菜单 -->
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
       <el-menu-item index="/">主页</el-menu-item>
       <el-menu-item index="/about">关于</el-menu-item>
     </el-menu>
+    <!-- 路由页面 -->
     <router-view/>
+    <!-- footer -->
+    
   </div>
 </template>
+
+<style lang="css">
+#header {
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+  font-size: 60px;
+  font-weight: 500;
+  color: #32A5FF;
+}
+</style>
 
 <script>
 export default {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="home">
-    Home
+    <p>02/03/20 Post : The IROS 2020 Organizing Committee is monitoring the evolving coronavirus situation. Conference submission deadlines remain firm as posted on the website. Please check back periodically for updates regarding possible changes to address the concerns.</p>
+    <p>02/03/20 Post : The IROS 2020 Organizing Committee is monitoring the evolving coronavirus situation. Conference submission deadlines remain firm as posted on the website. Please check back periodically for updates regarding possible changes to address the concerns.</p>
+    <p>02/03/20 Post : The IROS 2020 Organizing Committee is monitoring the evolving coronavirus situation. Conference submission deadlines remain firm as posted on the website. Please check back periodically for updates regarding possible changes to address the concerns.</p>
   </div>
 </template>
 
-<script>
+<style>
+.home {
+  padding: 40px;
+}
 
+.home p {
+  line-height: 20px;
+  font-size: 15px;
+  color: #999999;
+}
+</style>
+
+<script>
 export default {
   name: 'Home'
 }
